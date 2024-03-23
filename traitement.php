3ad2f81924bf96e700598877178e486f3aafb043
SamZoeDan <109177186+SamZoeDan@users.noreply.github.com>
2024-03-22 21:20:07 -0400

    modification de la connexion

--- traitement.php
+++ traitement.php
@@ -5,21 +5,15 @@ $password = "Autocar24";
 $dbname = "autocar";
 
 // Création de la connexion
-$conn = new mysqli($host, $username, $password, $dbname);
-
-// Vérifier la connexion
-if ($conn->connect_error) {
-  die("Échec de la connexion: " . $conn->connect_error);
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, NULL, NULL, NULL);
+if (!mysqli_real_connect($conn, $host, $username, $password, $dbname, 3306, NULL, MYSQLI_CLIENT_SSL)) {
+    die("Échec de la connexion : " . mysqli_connect_error());
 }
 
 // Vérifier si le formulaire a été soumis
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    // Collecter les données du formulaire
-    $nom = $_POST['nom'];
-    $prenom = $_POST['prenom'];
-    $telephone = $_POST['telephone'];
-    $date_naissance = $_POST['date_naissance'];
-    $voiture = $_POST['voiture'];
+    // Collecter et valider les données du formulaire ici
 
     // Préparer la requête SQL
     $stmt = $conn->prepare("INSERT INTO commande_formulaire (nom, prenom, telephone, date_naissance, voiture) VALUES (?, ?, ?, ?, ?)");
@@ -27,20 +21,16 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
         die("Erreur de préparation de la requête : " . $conn->error);
     }
 
-    // Lier les paramètres à la déclaration préparée en tant que chaînes
+    // Lier les paramètres et exécuter la requête
     $stmt->bind_param("sssss", $nom, $prenom, $telephone, $date_naissance, $voiture);
-
-    // Exécuter la requête SQL
     if ($stmt->execute() === TRUE) {
       echo "Nouvelle commande enregistrée avec succès.";
     } else {
       echo "Erreur lors de l'exécution de la requête : " . $stmt->error;
     }
 
-    // Fermer la requête préparée
     $stmt->close();
 }
 
-// Fermer la connexion
 $conn->close();
 ?>
