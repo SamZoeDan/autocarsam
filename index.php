44c8267411414c0216312939d4586d0917f14a55
SamZoeDan <109177186+SamZoeDan@users.noreply.github.com>
2024-03-22 23:07:31 -0400

    Update index.php

--- index.php
+++ index.php
@@ -1,3 +1,54 @@
+<?php
+$host = 'dbautocar.mysql.database.azure.com';
+$username = 'samoli';
+$password = 'Autocar24';
+$dbname = 'autocar';
+
+$message = ""; // Variable pour stocker les messages destinés à l'utilisateur
+
+// Création de la connexion
+$conn = mysqli_init();
+mysqli_ssl_set($conn, NULL, NULL, "C:\Users\samue\Downloads\DigiCertGlobalRootCA.crt.pem", NULL, NULL);
+mysqli_real_connect($conn, $host, $username, $password,  $dbname, 3307, MYSQLI_CLIENT_SSL);
+
+// Vérifier la connexion
+if(mysqli_connect_errno()) {
+    die("Échec de la connexion: " . mysqli_connect_error());
+}
+
+// Vérifier si le formulaire a été soumis
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    // Collecter les données du formulaire
+    $nom = $_POST['nom'];
+    $prenom = $_POST['prenom'];
+    $telephone = $_POST['telephone'];
+    $date_naissance = $_POST['date_naissance'];
+    $voiture = $_POST['voiture'];
+
+    // Préparer la requête SQL
+    $stmt = $conn->prepare("INSERT INTO commande_formulaire (nom, prenom, telephone, date_naissance, voiture) VALUES (?, ?, ?, ?, ?)");
+    if ($stmt === false) {
+        die("Erreur de préparation de la requête : " . $conn->error);
+    }
+
+    // Lier les paramètres à la déclaration préparée en tant que chaînes
+    $stmt->bind_param("sssss", $nom, $prenom, $telephone, $date_naissance, $voiture);
+
+    // Exécuter la requête SQL
+    if ($stmt->execute()) {
+        $message = "Nouvelle commande enregistrée avec succès.";
+    } else {
+        $message = "Erreur lors de l'exécution de la requête : " . $stmt->error;
+    }
+
+    // Fermer la requête préparée
+    $stmt->close();
+}
+
+// Fermer la connexion
+$conn->close();
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -7,14 +58,11 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css" integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="./css/form.css">
     <title>Home</title>
-
-    <link
-  rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"
-/>
-
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.min.css"/>
 </head>
 <body>
-    <form action="traitement.php" method="post" class="formulaire-achat">
+    <?php if($message) echo "<p>$message</p>"; ?>
+    <form action="index.php" method="post" class="formulaire-achat">
         <div class="input-group">
             <label for="nom">Nom:</label>
             <input type="text" id="nom" name="nom" required>
